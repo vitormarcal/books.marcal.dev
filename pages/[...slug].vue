c0946a16c2a6b1a519c8e754d17c73b25df9f662
Vítor Marçal <codehero@marcal.dev>
2025-01-12 13:21:23 -0300

    add first layout 🦋 with list page and book review

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const {data} = await useAsyncData(route.path, () => queryContent()
+    .where({
+      _path: route.path,
+    })
+    .findOne())
+
+</script>
+
 <template>
-  <main>
-    <ContentDoc />
-  </main>
+  <book-review :data="data"/>
 </template>
+
+<style scoped>
+
+</style>
---
+++ utils/date-utils.ts
@@ -0,0 +1,10 @@
+
+export const formatDate = (dateString: string): string | undefined => {
+    if (!dateString) return undefined
+    return new Date(dateString).toLocaleDateString('pt-BR', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        timeZone: 'UTC',
+    });
+};
